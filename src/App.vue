08e5dda426fc9b9917e6d2420b1e9da055223b79
kamalogudah <mcpaul2058@gmail.com>
2019-09-17 15:11:32 +0300

    Passing Data with Props and slots

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="container"></div>
+  <div id="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
 export default {
   data: function() {
     return {
       quotes: ['Just a Quote to see something'],
       maxQuotes: 10,
     };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
   },
 };
 </script>

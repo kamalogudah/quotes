0cbb29b4855a7c8bfd9d2fe2559af28462cfed2f
kamalogudah <mcpaul2058@gmail.com>
2019-09-17 18:53:30 +0300

    Adding Quotes and Custom Events

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -17,13 +17,14 @@ export default {
   data: function() {
     return {
       quote: '',
-    }
+    };
   },
   methods: {
     createNew() {
-      
-    }
-  }
+      this.$emit('quoteAdded', this.quote);
+      this.quote = '';
+    },
+  },
 };
 </script>
 
